572a6a1ac3dc14d9343ff9ed7ebc933700feb6d8
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-03-10 14:09:16 -0300

    Created the pages for home, films and nave. Routed them.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { FilmsComponent } from './pages/films/films.component';
+import { NaveComponent } from './pages/nave/nave.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'films',
+    component: FilmsComponent,
+  },
+  {
+    path: 'nave',
+    component: NaveComponent,
+  },
+];
---
+++ src/app/pages/nave/nave.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nave',
+  imports: [],
+  templateUrl: './nave.component.html',
+  styleUrl: './nave.component.css'
+})
+export class NaveComponent {
+
+}
