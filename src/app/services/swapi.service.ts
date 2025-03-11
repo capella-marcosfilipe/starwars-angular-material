a2a72b14190a08869b632a696bdc4dee7f37ad66
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-03-11 11:25:58 -0300

    Created a service to handle SWAPI

---
+++ src/app/services/swapi.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { RespostaAPI, Result } from './interfaces';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SwapiService {
+  api_url = 'https://swapi.dev/api';
+  constructor(private http: HttpClient) {}
+
+  getFilms(category: string): Observable<any> {
+    return this.http.get<RespostaAPI>(this.api_url + '/' + category);
+  }
+
+  search(category: string, value: string): Observable<Result> {
+    let params = new HttpParams().set('search', value);
+    return this.http.get<Result>(`${this.api_url}/${category}/`, { params });
+  }
+}
