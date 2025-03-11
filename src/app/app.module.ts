9e6c4c07d6e679e9e4093903d67059b8011f04d9
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-03-11 11:26:31 -0300

    Created a MatTable to display the SW films

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { SpacecraftComponent } from './pages/spacecraft/spacecraft.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FilmsComponent } from './pages/films/films.component';
 
+import { MatTableModule } from '@angular/material/table';
+
 @NgModule({
-  declarations: [AppComponent, SpacecraftComponent, HomeComponent, FilmsComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [provideHttpClient()],
+  declarations: [
+    AppComponent,
+    SpacecraftComponent,
+    HomeComponent,
+    FilmsComponent,
+  ],
+  imports: [BrowserModule, AppRoutingModule, MatTableModule],
+  providers: [provideHttpClient(withFetch())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/pages/films/films.component.ts
+++ src/app/pages/films/films.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { SwapiService } from '../../services/swapi.service';
+import { FilmInfo } from '../../services/interfaces';
 
 @Component({
   selector: 'app-films',
   standalone: false,
   templateUrl: './films.component.html',
-  styleUrl: './films.component.css'
+  styleUrl: './films.component.css',
 })
 export class FilmsComponent {
+  displayedColumns: string[] = ['episode_id', 'title', 'release_date'];
+  filmsList: FilmInfo[] = [];
 
+  constructor(private service: SwapiService) {}
+
+  ngOnInit(): void {
+    this.feedFilms();
+  }
+
+  feedFilms(): void {
+    this.service
+      .getFilms('films')
+      .subscribe((filmsList) => (this.filmsList = filmsList.results));
+  }
 }
