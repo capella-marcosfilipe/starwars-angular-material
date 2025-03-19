c6601750a95f7214b836032910065c4ac450b2bf
Marcos Filipe Capella <marcoscapella@outlook.com>
2025-03-19 10:04:50 -0300

    Created the search form and table for startships

--- src/app/pages/starships/starships.component.ts
+++ src/app/pages/starships/starships.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { SwapiService } from '../../services/swapi.service';
+import { ApiResponse, Starship } from '../../services/interfaces';
 
 @Component({
   selector: 'app-starships',
   standalone: false,
   templateUrl: './starships.component.html',
-  styleUrl: './starships.component.css'
+  styleUrl: './starships.component.css',
 })
 export class StarshipsComponent {
+  starshipsList: Starship[] = [];
+  searchTerm: string = '';
+  displayedColumns: string[] = ['name', 'model', 'manufacturer', 'passengers'];
 
+  constructor(private service: SwapiService) {}
+
+  ngOnInit(): void {
+    this.feedStarships();
+  }
+
+  feedStarships(searchQuery: string = ''): void {
+    const url = searchQuery ? `starships/?search=${searchQuery}` : 'starships';
+    this.service
+      .fetchData<Starship>(url)
+      .subscribe(
+        (response: ApiResponse<Starship>) =>
+          (this.starshipsList = response.results)
+      );
+  }
 }
